5a32738dec24b0324de6b0aaa7120d9b0905e67d
Melissa Smith <melissasmith@Melissas-MacBook-Pro.local>
2016-06-16 12:13:21 -0700

    react part one first component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
+// importing a library called React
 import React from 'react';
+// need to import ReactDOM to insert component into the DOM
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should producer some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// declaring a constant called App and assigning it a function
+// this creates a class -> an instance of a component
+// const App = function() {
+//   return <div>Hi!</div>;
+// }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// This takes the function above and moves it into ES6 syntax
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+// This will cause an error because we're not passing the JSX
+// ReactDOM.render(App);
+
+// This will cause an error "Target container is not a DOM element" Because we're not telling it where to put it.
+// ReactDOM.render(<App />);
+
+// Take this component, render it, and put it into this specific node (or element). Need to pass the JSX & the reference to the container
+ReactDOM.render(<App />, document.querySelector('.container'));
